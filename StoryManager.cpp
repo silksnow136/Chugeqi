ef108c61359ff393fbdec3cb3ab58b94e80e0d13
Nefer946 <1405106796@qq.com>
2026-08-28 11:44:40 +0800

    对话系统制作完成，对话内容待装载

--- StoryManager.cpp
+++ StoryManager.cpp
@@ -72,6 +72,7 @@ void talkScene_01() {
 	cout << "1. 小卒a\n";
 	cout << "2. 虞姬\n";
 	cout << "3. 返回\n";
+	cout << "\n输入w继续游戏\n";
 
 	cout << "\n> ";
 }
@@ -93,9 +94,10 @@ void talkScene_021() {
 	cout << "              对话\n";
 	cout << "================================\n";
 
-	cout << "1. \n";
-	cout << "2. \n";
+	cout << "1. 王翦\n";
+	cout << "2. 汉军\n";
 	cout << "3. 返回\n";
+	cout << "\n输入w继续游戏\n";
 
 	cout << "\n> ";
 }
@@ -105,9 +107,9 @@ void talkScene_022() {
 	cout << "              对话\n";
 	cout << "================================\n";
 
-	cout << "1. \n";
-	cout << "2. \n";
-	cout << "3. 返回\n";
+	cout << "1. 副将\n";
+	cout << "2. 返回\n";
+	cout << "\n输入w继续游戏\n";
 
 	cout << "\n> ";
 }
@@ -117,9 +119,12 @@ void talkScene_03() {
 	cout << "              对话\n";
 	cout << "================================\n";
 
-	cout << "1. 项羽\n";
-	cout << "2. 虞姬\n";
-	cout << "3. 返回\n";
+	cout << "1. 赤泉侯\n";
+	cout << "2. 秦时月\n";
+	cout << "3. 钟离昧\n";
+	cout << "4. 二十八骑\n";
+	cout << "5. 返回\n";
+	cout << "\n输入w继续游戏\n";
 
 	cout << "\n> ";
 }
@@ -129,9 +134,13 @@ void talkScene_04() {
 	cout << "              对话\n";
 	cout << "================================\n";
 
-	cout << "1. 项羽\n";
-	cout << "2. 虞姬\n";
-	cout << "3. 返回\n";
+	cout << "1. 韩信\n";
+	cout << "2. 汉军\n";
+	cout << "3. 钟离昧\n";
+	cout << "4. 二十八骑\n";
+	cout << "5. 乌江亭长\n";
+	cout << "6. 返回\n";
+	cout << "\n输入w继续游戏\n";
 
 	cout << "\n> ";
 }
@@ -154,13 +163,48 @@ void talk_character_contnt_020(int current_character) {
 
 }
 void talk_character_contnt_021(int current_character) {
+	switch (current_character) {
+	case 1:
+		//王翦
+		cout << "王翦的对话没写" << "\n";
+		break;
 
+	case 2:
+		//汉军
+		cout << "汉军的对话没写" << "\n";
+		break;
+	}
 }
 void talk_character_contnt_022(int current_character) {
+	switch (current_character) {
+	case 1:
+		//副将
+		cout << "副将的对话没写" << "\n";
+		break;
 
+	}
 }
 void talk_character_contnt_03(int current_character) {
+	switch (current_character) {
+	case 1:
+		//赤泉侯
+		cout << "赤泉侯的对话没写" << "\n";
+		break;
 
+	case 2:
+		//秦时月
+		cout << "秦时月的对话没写" << "\n";
+		break;
+
+	case 3:
+		//钟离昧
+		cout << "钟离昧的对话没写" << "\n";
+		break;
+	case 4:
+		//二十八骑
+		cout << "二十八骑的对话没写" << "\n";
+		break;
+	}
 }
 void talk_character_contnt_04(int current_character) {
 
